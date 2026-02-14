a4a14286a66b722239d6dbf8bcbce509c8cd49b2
Piyush <Piyushchaudhary564449@gmail.com>
2026-02-14 22:10:40 +0530

    Project Update: Added new modules/tests

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl:'https://opensource-demo.orangehrmlive.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
